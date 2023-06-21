26de5108ad94b08674d53989c302d1bd032b3e94
WalobwaD <walobwadan@gmail.com>
2023-06-21 20:33:15 +0300

    Project Initialization

---
+++ pages/login/index.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="main">
+        <div>
+            <h1>Nairobi Sausages</h1>
+        </div>
+        <form class="login-form">
+            <div class="labels">
+                <label for="email">Enter Your Email Address</label>
+                <input type="text" id="email" name="email" placeholder="Email" />
+            </div>
+
+            <div class="labels">
+                <label for="password">Enter You Password</label>
+                <input type="password" id="password" name="password" placeholder="Password" />
+            </div>
+
+
+            <button class="login-button">Login</button>
+
+            <div class="text">
+                <p>Don't Have an Account? <a>Register</a></p>
+                <p>Forgot Password</p>
+            </div>
+        </form>
+    </div>
+</template>
+
+<script setup>
+</script>
+<style scoped lang="scss">
+@use '~/assets/scss/login.scss';
+</style>

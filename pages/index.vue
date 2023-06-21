276e4fe56a1ea1531a4994cc90c3db50369d7495
WalobwaD <walobwadan@gmail.com>
2023-06-21 21:21:00 +0300

    Added images

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,12 @@
 <template>
     <div>
         Hello World
+        <NuxtLink to="/login">Login</NuxtLink>
     </div>
 </template>
 
 <script>
+
 </script>
 
 <style scoped lang="scss">
--- pages/login/index.vue
+++ pages/login/index.vue
@@ -1,27 +1,33 @@
 <template>
     <div class="main">
-        <div>
-            <h1>Nairobi Sausages</h1>
+        <div class="barner">
+            <img src="/Kenyancheesesausages.jpg"/>
         </div>
-        <form class="login-form">
-            <div class="labels">
-                <label for="email">Enter Your Email Address</label>
-                <input type="text" id="email" name="email" placeholder="Email" />
+        <div class="logo-form">
+            <div class="nairobi-logo">
+                <img src="/nairobilogo.webp"/>
             </div>
+            <form class="login-form">
+                <div class="labels">
+                    <label for="email">Enter Your Email Address</label>
+                    <input type="text" id="email" name="email" placeholder="Email" />
+                </div>
+    
+                <div class="labels">
+                    <label for="password">Enter You Password</label>
+                    <input type="password" id="password" name="password" placeholder="Password" />
+                </div>
+    
+    
+                <button class="login-button">Login</button>
+    
+                <div class="text">
+                    <p>Don't Have an Account? <a>Register</a></p>
+                    <p>Forgot Password</p>
+                </div>
+            </form>
+        </div>
 
-            <div class="labels">
-                <label for="password">Enter You Password</label>
-                <input type="password" id="password" name="password" placeholder="Password" />
-            </div>
-
-
-            <button class="login-button">Login</button>
-
-            <div class="text">
-                <p>Don't Have an Account? <a>Register</a></p>
-                <p>Forgot Password</p>
-            </div>
-        </form>
     </div>
 </template>
 
